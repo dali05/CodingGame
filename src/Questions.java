c7edde6c76739407bb126266d9cde07de3c09432
Ayoub MKHARBACH <ayoub.mkharbach@gmail.com>
2020-04-07 15:02:10 +0200

    Add new solutions

---
+++ src/Questions.java
@@ -0,0 +1,13 @@
+public class Questions {
+
+    public static void main(String[] args) {
+
+        /*
+        Signed right shift
+        010 --> 001
+         */
+        System.out.println(2>>1);
+
+
+    }
+}
